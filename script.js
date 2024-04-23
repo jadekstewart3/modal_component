2a6c2d79a60b682065117d54caf510ba08a829de
Jade Stewart <jadekstewart3@gmail.com>
2024-04-23 16:45:24 -0600

    Add event listener for keydown event

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ for(let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//keypress events- are global events
+
+document.addEventListener('keydown', function(e) {
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
