b1915bc03f06a0c79eaa0639c4700e9a66a454e7
Jade Stewart <jadekstewart3@gmail.com>
2024-04-23 16:35:58 -0600

    Refactor openModal event listener

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
-for(let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function(){
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-});
-
 const closeModal = function(){
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
-btnCloseModal.addEventListener('click', closeModal);
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
+for(let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
